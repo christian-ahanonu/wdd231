38cd3c1312bd8d391282c0fcba30b2ed507b53cd
christian-ahanonu <ahanonuchristian116@gmail.com>
2024-10-29 19:18:07 +0100

    Created site plan for Finder website

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,5 @@
+
+
+document.getElementById('currentyear').textContent = `${new Date().getFullYear()}`;
+
+document.getElementById('lastmodified').textContent = `Last Modification: ${document.lastModified}`;
